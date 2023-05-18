ac8f4d4c25fcb94e42024bb3d4d64026eb2f3da4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-18 15:02:28 +0000

    Task2, Handle command lines with arguments, fix memory bugs

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 #include <stdio.h>	  /*stdin*/
 #include <sys/wait.h> /*wait*/
 #include <errno.h>
+#include <string.h> /*strlen*/
 
-int main(int ac, char **av)
+int main(int ac, char **argv)
 {
 	char *cmd = NULL;
 	size_t n = 0;
@@ -12,40 +13,78 @@ int main(int ac, char **av)
 	pid_t child_pid = 0;
 	int status = 0;
 	const char prompt[] = "#nuux$ ";
-	char *av[] = {cmd, NULL};
-	(void)ac;
+	char **av = NULL;
+	(void)ac, (void)argv;
+	char *token;
+	char *delim = " ";
+	int tokens_total = 0;
+	int i = 0;
 
 	while (1)
 	{
+		/*allocate memory for the command and argument array*/
+		av = malloc(2 * sizeof(char *));
+		av[0] = NULL;
+		av[1] = NULL;
 		write(STDOUT_FILENO, prompt, sizeof(prompt));
 		n = 0;
+		i = 0;
+		tokens_total = 0;
 		nread = getline(&cmd, &n, stdin);
 		/* check getline error and exit with error (also ctrl+d)*/
 		if (nread == -1)
-			exit(1);
+		{
+			free(cmd);
+			free(av);
+			/*perror("getline");*/
+			exit(EXIT_FAILURE);
+		}
 		/*user input remplace the new line with end of line*/
 		cmd[nread - 1] = '\0';
-		if (access(cmd, F_OK) == -1)
-			perror(av[0]);
+		/*make array of cmd and argument*/
+		token = strtok(cmd, delim);
+		while (token)
+		{
+			av = realloc(av, (tokens_total + 2) * sizeof(char *));
+			if (av == NULL)
+			{
+				while (av[i])
+				{
+					/*printf("Realloc error:%d %s\n", tokens_total, av[i]);*/
+					free(av[i]);
+					i++;
+				}
+				free(av);
+				free(cmd);
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+			av[tokens_total] = realloc(av[tokens_total], (strlen(token) + 1) * sizeof(char));
+			strcpy(av[tokens_total], token);
+			tokens_total++;
+			token = strtok(NULL, delim);
+		}
+		av[tokens_total] = NULL;
+		/*check if file exist before fork*/
+		if (access(av[0], F_OK) == -1)
+			perror("access");
 		else
 		{
 			child_pid = fork();
-			if (child_pid == 0 && execve(cmd, av, NULL) != 0)
-				perror(av[0]);
+			if (child_pid == 0 && execve(av[0], av, NULL) == -1)
+				perror("execve");
+
+			wait(&status);
 		}
-		wait(&status);
+		i = 0;
+		while (av[i])
+		{
+			/*printf("End of loop : %d %s\n", tokens_total, av[i]);*/
+			free(av[i]);
+			i++;
+		}
+		free(cmd);
+		free(av);
 	}
-	free(cmd);
 	return (0);
 }
-
-/*
-display prompt (always on after each command)
-give user input
-	get input (it end with\n) (simple command 1 word)
-		check if it exist
-			if not print error
-			if it exist do a fork and execve
-case to test :
-	ctrl+d : exit
-*/
