2ae55a91a84437e3ea78ea9fc0a228e23d899844
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-18 19:57:45 +0000

    Task1,2 : free memory before CTRL+C exit, also add argv[0] to global variable, free av now is function

--- LOG.md
+++ LOG.md
@@ -4,3 +4,9 @@
 	we finish resources and concept page exercices
 	we read and discuss all tasks with writing note for each one
 	start working on task 1
+# 2023-05-17
+	building simple shell take 1 command
+# 2023-05-17
+	fix memory problem
+	handle command with argument
+	handle error
--- main.c
+++ main.c
@@ -4,88 +4,106 @@
 #include <sys/wait.h> /*wait*/
 #include <errno.h>
 #include <string.h> /*strlen*/
+#include <signal.h>
 
+typedef struct
+{
+	char **av;
+	char *cmd;
+	char *shell_name;
+	int main_loop;
+} data;
+data global_data;
+void free_av()
+{
+	int i = 0;
+	if (!global_data.av)
+		return;
+	while (global_data.av[i])
+	{
+		/*printf("End of loop : %d %s\n", tokens_total, av[i]);*/
+		free(global_data.av[i]);
+		i++;
+	}
+	free(global_data.av);
+	/*printf("freed av...\n");*/
+}
+void handler_sigint(int sig)
+{
+	if (global_data.cmd)
+	{
+		free(global_data.cmd);
+		/*printf("free cmd...\n");*/
+	}
+	free_av();
+	/*printf("Received SIGINT signal. Exiting...\n");*/
+	exit(sig);
+}
 int main(int ac, char **argv)
 {
-	char *cmd = NULL;
+	global_data.cmd = NULL;
+	global_data.av = NULL;
+	global_data.shell_name = argv[0];
+	global_data.main_loop = 1;
 	size_t n = 0;
 	ssize_t nread;
 	pid_t child_pid = 0;
-	int status = 0;
+	int status = 0, tokens_total = 0;
 	const char prompt[] = "#csisfun$ ";
-	char **av = NULL;
 	(void)ac, (void)argv;
-	char *token;
-	char *delim = " ";
-	int tokens_total = 0;
-	int i = 0;
+	char *token, *delim = " ";
 
-	while (1)
+	signal(SIGINT, handler_sigint);
+	while (global_data.main_loop)
 	{
-		/*allocate memory for the command and argument array*/
-		av = malloc(2 * sizeof(char *));
-		av[0] = NULL;
-		av[1] = NULL;
+		global_data.av = malloc(2 * sizeof(char *));
+		global_data.av[0] = NULL;
+		global_data.av[1] = NULL;
 		write(STDOUT_FILENO, prompt, sizeof(prompt));
 		n = 0;
-		i = 0;
 		tokens_total = 0;
-		nread = getline(&cmd, &n, stdin);
+		nread = getline(&global_data.cmd, &n, stdin);
 		/* check getline error and exit with error (also ctrl+d)*/
 		if (nread == -1)
 		{
-			free(cmd);
-			free(av);
+			free(global_data.cmd);
+			free(global_data.av);
 			/*perror("getline");*/
 			exit(EXIT_FAILURE);
 		}
 		/*user input remplace the new line with end of line*/
-		cmd[nread - 1] = '\0';
+		global_data.cmd[nread - 1] = '\0';
 		/*make array of cmd and argument*/
-		token = strtok(cmd, delim);
+		token = strtok(global_data.cmd, delim);
 		while (token)
 		{
-			av = realloc(av, (tokens_total + 2) * sizeof(char *));
-			if (av == NULL)
+			global_data.av = realloc(global_data.av, (tokens_total + 2) * sizeof(char *));
+			if (global_data.av == NULL)
 			{
-				while (av[i])
-				{
-					/*printf("Realloc error:%d %s\n", tokens_total, av[i]);*/
-					free(av[i]);
-					i++;
-				}
-				free(av);
-				free(cmd);
-				perror("realloc");
+				free_av();
+				free(global_data.cmd);
+				perror(global_data.shell_name);
 				exit(EXIT_FAILURE);
 			}
-			av[tokens_total] = realloc(av[tokens_total], (strlen(token) + 1) * sizeof(char));
-			strcpy(av[tokens_total], token);
+			global_data.av[tokens_total] = realloc(global_data.av[tokens_total], (strlen(token) + 1) * sizeof(char));
+			strcpy(global_data.av[tokens_total], token);
 			tokens_total++;
 			token = strtok(NULL, delim);
 		}
-		av[tokens_total] = NULL;
+		global_data.av[tokens_total] = NULL;
 		/*check if file exist before fork*/
-		if (access(av[0], F_OK) == -1)
-			perror("access");
+		if (access(global_data.av[0], F_OK) == -1)
+			perror(global_data.shell_name);
 		else
 		{
 			child_pid = fork();
-			if (child_pid == 0 && execve(av[0], av, NULL) == -1)
-				perror("execve");
+			if (child_pid == 0 && execve(global_data.av[0], global_data.av, NULL) == -1)
+				perror(global_data.shell_name);
 
 			wait(&status);
 		}
-		i = 0;
-		while (av[i])
-		{
-			/*printf("End of loop : %d %s\n", tokens_total, av[i]);*/
-			free(av[i]);
-			i++;
-		}
-		free(cmd);
-		free(av);
+		free_av();
+		free(global_data.cmd);
 	}
 	return (0);
 }
-
