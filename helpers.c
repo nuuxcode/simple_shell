a8709ce8bb16744cc63c6423006fd9d9544e58eb
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-05-26 00:19:03 +0000

    going back to origin realloc temopraly

--- helpers.c
+++ helpers.c
@@ -61,7 +61,7 @@ void split(data *d, const char *delim)
 	token = strtok(d->cmd, delim);
 	while (token)
 	{
-		d->av = _realloc(d->av, (ntoken + 2) * sizeof(char *));
+		d->av = realloc(d->av, (ntoken + 2) * sizeof(char *));
 		if (d->av == NULL)
 			goto free;
 		d->av[ntoken] = _strdup(token);
